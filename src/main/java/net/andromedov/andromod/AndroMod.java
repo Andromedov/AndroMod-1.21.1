b14a5b1438fcec82de2f40d43d11b57b012212d5
Andromedov <andromedov.mod@gmail.com>
2024-11-30 17:07:33 +0200

    Initial commit

---
+++ src/main/java/net/andromedov/andromod/AndroMod.java
@@ -0,0 +1,15 @@
+package net.andromedov.andromod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AndroMod implements ModInitializer {
+	public static final String MOD_ID = "andromod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
